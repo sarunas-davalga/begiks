e9fd2618ed98b0046d655365b987fce72b67a159
simpl <sarunas@abneta.lt>
2015-04-02 11:43:45 +0300

    Vagrant and test app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-// TODO write the app :)
+var express = require("express"),
+    app = express();
+
+app.get("/", function (req, res) {
+    res.json({hello: 1, env: process.env});
+});
+
+app.listen(3000, function () {
+    console.log("Started.");
+});
