b52583168d57c55a975dc5c88e85bfc8975211e3
simpl <sarunas@abneta.lt>
2015-04-09 17:03:30 +0300

    deploy via grunt

---
+++ grunt/deploy.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var BClient = require("../client/client"),
+    path = require("path");
+
+module.exports = function (grunt) {
+    grunt.registerTask("begiks", "Deploy application to begiks server", function () {
+        var defaultConfig = {
+            name: "my-app",
+            host: "127.0.0.1",
+            port: "3000",
+            path: path.resolve(),
+            exclude: []
+        };
+
+        grunt.util._.defaults(grunt.config.data.begiks, defaultConfig);
+
+        var done = this.async(),
+            cfg = grunt.config.data.begiks,
+            client = new BClient("http://" + cfg.host + ":" + cfg.port);
+
+        client.deployApp(cfg.name, {path: cfg.path, exclude: cfg.exclude})
+            .then(done).catch(grunt.fail.error).done();
+    });
+};
